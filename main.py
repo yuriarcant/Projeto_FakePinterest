e65eec60f69f3fc3cce9c6cc856463d9097d962b
yuriarcant <yuriarcant@gmail.com>
2024-05-08 14:34:22 -0300

    adicionei requirements.txt

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ from fakepinterest import app
 
 if __name__  == '__main__': #colocando o site no ar
     app.run(debug= False)
+
+
+#caso queira verificar todas as bibliotecas basta rodar um pip freeze, e caso queira salvar isso em um arquivo txt rode o comando com > requirements.txt
+
+#comandos para atualizar, projeto no github para todas vez que, modificar o projeto.
